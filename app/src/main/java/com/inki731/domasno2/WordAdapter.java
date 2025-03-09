2a99e80a7c3cb12657b083fc11290bad77a385b8
achkovski-kiril <achkovski.kiril@uklo.edu.mk>
2025-03-09 19:20:02 +0100

    Update WordAdapter.java

--- app/src/main/java/com/inki731/domasno2/WordAdapter.java
+++ app/src/main/java/com/inki731/domasno2/WordAdapter.java
@@ -5,16 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<String> {
-    private LayoutInflater inflater;
 
-    public WordAdapter(Context context, ArrayList<String> words) {
+    private LayoutInflater inflater;
+    private OnWordDeleteListener deleteListener;
+
+    public interface OnWordDeleteListener {
+        void onWordDelete(int position, String word);
+    }
+
+    public WordAdapter(Context context, ArrayList<String> words, OnWordDeleteListener listener) {
         super(context, 0, words);
         this.inflater = LayoutInflater.from(context);
+        this.deleteListener = listener;
     }
 
     @Override
@@ -23,10 +31,17 @@ public class WordAdapter extends ArrayAdapter<String> {
             convertView = inflater.inflate(R.layout.word_item, parent, false);
         }
 
-        String word = getItem(position);
+        final String word = getItem(position);
         if (word != null) {
             TextView wordTextView = convertView.findViewById(R.id.wordTextView);
             wordTextView.setText(word);
+
+            ImageButton deleteButton = convertView.findViewById(R.id.deleteButton);
+            deleteButton.setOnClickListener(v -> {
+                if (deleteListener != null) {
+                    deleteListener.onWordDelete(position, word);
+                }
+            });
         }
 
         return convertView;
